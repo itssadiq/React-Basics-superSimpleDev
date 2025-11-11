450997b04b1ca1acd9ee0845461e818d8aac3fc2
itssadiq <sadiqalishaikh009@gmail.com>
2025-11-11 23:29:30 +0500

    Updated all anchor tags and used new react Links

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,16 +1,18 @@
 import { Header } from "../components/Header";
+import { Link } from "react-router";
 import "./TrackingPage.css";
 
 export function TrackingPage() {
   return (
     <>
       <title>Tracking</title>
+      <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
       <Header />
       <div className="tracking-page">
         <div className="order-tracking">
-          <a className="back-to-orders-link link-primary" href="/orders">
+          <Link className="back-to-orders-link link-primary" to="/orders">
             View all orders
-          </a>
+          </Link>
 
           <div className="delivery-date">Arriving on Monday, June 13</div>
 
