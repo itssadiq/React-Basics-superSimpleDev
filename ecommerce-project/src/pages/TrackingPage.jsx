d6eecc082e6f1661a68ffafe249fe205f96b8c70
itssadiq <sadiqalishaikh009@gmail.com>
2025-11-19 13:05:31 +0500

    Displayed total quantity on checkout and and fixed prop error on track page header.

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -2,12 +2,12 @@ import { Header } from "../components/Header";
 import { Link } from "react-router";
 import "./TrackingPage.css";
 
-export function TrackingPage() {
+export function TrackingPage({ cart }) {
   return (
     <>
       <title>Tracking</title>
       <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
-      <Header />
+      <Header cart={cart} />
       <div className="tracking-page">
         <div className="order-tracking">
           <Link className="back-to-orders-link link-primary" to="/orders">
